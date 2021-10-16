f0ca83ccddd512a2777e38a8375ffe909880d34e
juantautiva <juantautiva@gmail.com>
2021-10-16 12:54:16 -0500

    Correción de problema multRouter

--- apis/helloworld/app.js
+++ apis/helloworld/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var healthRouter = require('./routes/healthRouter');
-var sumaRouter = require("./routes/multiplicacion");
+var multRouter = require("./routes/multiplicacion");
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/healthCheck', healthRouter);
-app.use("/multiplicacion", sumaRouter);
+app.use("/multiplicacion", multRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
