387857d2d5c2947a724b6d80c57ebacf6ef9d1b6
juantautiva <juantautiva@gmail.com>
2021-10-16 12:48:31 -0500

    Cambio de APPs - Operación multiplicación.

--- apis/helloworld/app.js
+++ apis/helloworld/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var healthRouter = require('./routes/healthRouter');
+var sumaRouter = require("./routes/multiplicacion");
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/healthCheck', healthRouter);
+app.use("/multiplicacion", sumaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
